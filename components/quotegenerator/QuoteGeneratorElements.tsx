7053b6e54c288a47ae4343cd2463bd30884af555
Hoareau.Steevy <sty.hoareau@gmail.com>
2023-05-28 20:17:47 +0200

    fix support styled component

---
+++ components/quotegenerator/QuoteGeneratorElements.tsx
@@ -0,0 +1,23 @@
+"use client";
+import styled from "styled-components";
+import Link from "next/link";
+import Image from "next/image";
+
+export const GradientBackgroundCon = styled.div`
+  background-size: 400% 400%;
+  background-image: linear-gradient(to right, #e66465, #9198e5);
+  animation: gradient 6s ease infinite;
+  height: 100vh;
+  width: 100vw;
+  @keyframes gradient {
+    0% {
+      background-position: 0% 50%;
+    }
+    50% {
+      background-position: 100% 50%;
+    }
+    100% {
+      background-position: 0% 50%;
+    }
+  }
+`;
