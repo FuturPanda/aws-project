1356a926f9a46f81ed92540f326e14f3b38194e0
Hoareau.Steevy <sty.hoareau@gmail.com>
2023-05-31 19:29:24 +0200

    Update nodeserveur.js

--- serveur/nodeserveur.js
+++ serveur/nodeserveur.js
@@ -10,20 +10,13 @@ const fetch = require("node-fetch");
 // 6. SVG -> image as png (/base 64 in lambda)
 // 7. Send the lines to the front end.
 
-const getRandomQuote = async (apiUrl) => {
-  const response = await fetch(apiUrl);
-  const quoteData = await response.json();
-  const quoteText = quoteData[0].q;
-  const quoteAuthor = quoteData[0].a;
-  console.log(`${quoteText} \n${quoteAuthor}`);
+const makeTextSpan = (quoteText, linebreak, width) => {
   const words = quoteText.split(" ");
-  const width = 750;
-  const height = 483;
   let newText = "";
   let tspanElements = "";
   for (let i = 0; i < words.length; i++) {
     newText += words[i] + " ";
-    if ((i + 1) % 4 === 0) {
+    if ((i + 1) % linebreak === 0) {
       tspanElements += `<tspan x="${width / 2}" dy="1.2em">${newText}</tspan>`;
       newText = "";
     }
@@ -31,7 +24,95 @@ const getRandomQuote = async (apiUrl) => {
   if (newText !== "") {
     tspanElements += `<tspan x="${width / 2}" dy="1.2em">${newText}</tspan>`;
   }
-  console.log(tspanElements);
+  return tspanElements;
 };
 
-getRandomQuote(apiUrl);
+const getRandomBackground = (backgroundImages) => {
+  const randomIndex = Math.floor(Math.random() * backgroundImages.length);
+  return backgroundImages[randomIndex];
+};
+
+const makeSVG = async (
+  height,
+  width,
+  backgroundImage,
+  tspanElements,
+  quoteAuthor
+) => {
+  const svgImage = `
+          <svg width="${width}" height="${height}">
+              <style>
+                 .title { 
+                   fill: #ffffff; 
+                  font-size: 20px; 
+                     font-weight: bold;
+                }
+               .quoteAuthorStyles {
+                     font-size: 35px;
+                    font-weight: bold;
+                   padding: 50px;
+              }
+                .footerStyles {
+                  font-size: 20px;
+                     font-weight: bold;
+                    fill: lightgrey;
+                   text-anchor: middle;
+                  font-family: Verdana;
+              }
+              </style>
+              <g>
+                  <rect x="0" y="0" width="${width}" height="auto"></rect>
+                     <text id="lastLineOfQuote" x="375" y="80" font-family="Verdana" font-size="35" fill="white" text-anchor="middle">
+                        ${tspanElements}
+                    <tspan class="quoteAuthorStyles" x="375" dy="1.8em">- ${quoteAuthor}</tspan>
+               </text>
+                </g>
+              <text x="${width / 2}" y="${
+    height - 10
+  }" class="footerStyles">Developed by @BrianHHough | Quotes from ZenQuotes.io</text>
+          </svg>
+        `;
+  const timestamp = new Date().toLocaleString().replace(/[^\d]/g, "");
+  const svgBuffer = Buffer.from(svgImage);
+
+  const imagePath = `./finals/quote-card_${timestamp}.png`;
+  const image = await sharp(backgroundImage)
+    .composite([
+      {
+        input: svgBuffer,
+        top: 0,
+        left: 0,
+      },
+    ])
+    .toFile(imagePath);
+};
+
+const getQuotesApi = async (apiUrl) => {
+  const response = await fetch(apiUrl);
+  const quoteData = await response.json();
+  const quoteText = quoteData[0].q;
+  const quoteAuthor = quoteData[0].a;
+  return { quoteText, quoteAuthor };
+};
+
+const constructSVGFromQuote = async (apiUrl, width, height, linebreak) => {
+  const { quoteText, quoteAuthor } = await getQuotesApi(apiUrl);
+  const tspanElements = makeTextSpan(quoteText, linebreak, width);
+
+  const backgroundImages = [
+    "./backgrounds/Aubergine.png",
+    "./backgrounds/Mantle.png",
+    "./backgrounds/Midnight-City.png",
+    "./backgrounds/Orangey.png",
+  ];
+  const backgroundImage = getRandomBackground(backgroundImages);
+
+  const svgImage = makeSVG(
+    height,
+    width,
+    backgroundImage,
+    tspanElements,
+    quoteAuthor
+  );
+};
+constructSVGFromQuote(apiUrl, 750, 483, 4);
